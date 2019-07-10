2856ca4069a42979e0a4bcea85d592321339b43f
WellintonCamboim <wellinton.camboim@gmail.com>
2019-07-10 16:18:26 -0300

    JPA, Spring Data e Hibernate-Valid

--- tw-gerenciador-tarefas/src/main/java/com/projetospringtreinaweb/twgerenciadortarefas/controllers/TarefasController.java
+++ tw-gerenciador-tarefas/src/main/java/com/projetospringtreinaweb/twgerenciadortarefas/controllers/TarefasController.java
@@ -1,8 +1,14 @@
 package com.projetospringtreinaweb.twgerenciadortarefas.controllers;
 
+import java.util.Date;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -36,10 +42,52 @@ public class TarefasController {
 		return mv;
 	}
 	
-//	@PostMapping("/inserir")
-//	public String inserir(Tarefa tarefa) {
-//		repositoryTarefa.save(tarefa);
-//		return "redirect:/tarefas/listar";
-//	}
+	@PostMapping("/inserir")
+	public ModelAndView inserir(@Valid Tarefa tarefa, BindingResult result) {
+		ModelAndView mv = new ModelAndView();
+		if (tarefa.getDataExpiracao() == null) {
+			result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida", "A data de expiração é obrigatória.");
+		}else {
+			if(tarefa.getDataExpiracao().before(new Date())) {
+			result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida", "A data de expiração não pode ser anterior à data atual.");
+			}
+		}
+		if (result.hasErrors()) {
+			mv.setViewName("tarefas/inserir");
+			mv.addObject(tarefa);
+		}else {
+			mv.setViewName("redirect:tarefas/listar");
+			repositoryTarefa.save(tarefa);
+		}
+		return mv;
+	}
+	@GetMapping("/alterar/{id}")
+	public ModelAndView alterar(@PathVariable("id") Long id) {
+		ModelAndView mv = new ModelAndView();
+		Tarefa tarefa = repositoryTarefa.getOne(id);
+		mv.addObject("tarefa", tarefa);
+		return mv;
+	}
+	
+
+	@PostMapping("/alterar")
+	public ModelAndView alterar(@Valid Tarefa tarefa, BindingResult result) {
+		ModelAndView mv = new ModelAndView();
+		if (tarefa.getDataExpiracao() == null) {
+			result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida", "A data de expiração é obrigatória.");
+		}else {
+			if(tarefa.getDataExpiracao().before(new Date())) {
+			result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida", "A data de expiração não pode ser anterior à data atual.");
+			}
+		}
+		if (result.hasErrors()) {
+			mv.setViewName("tarefas/alterar");
+			mv.addObject(tarefa);
+		}else {
+			mv.setViewName("redirect:tarefas/listar");
+			repositoryTarefa.save(tarefa);
+		}
+		return mv;
+	}
 
 }
